c6ab892636a97cd07c6693f14b5ee960c4917ae1
Yasril Imam <gallerycreatifease@gmail.com>
2020-07-23 14:33:36 +0800

    Initial Commit

---
+++ sispakoapps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sispakoapps
+//
+//  Created by Devfx on 23/07/20.
+//  Copyright © 2020 Devfx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
